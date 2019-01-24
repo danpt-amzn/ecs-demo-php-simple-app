2edb9b317ee07ca9e6f557d85fac489de773eb53
danpt-amzn <45993622+danpt-amzn@users.noreply.github.com>
2019-01-24 17:17:54 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>ECS Microservice Demo App</h1>
-                <h2>Congratulations</h2>
+                <h2>Welcome</h2>
                 <p>Web App running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
