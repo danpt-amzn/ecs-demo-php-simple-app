9729d67303f13c9bc79fec5ba4691669403ea861
danpt-amzn <45993622+danpt-amzn@users.noreply.github.com>
2019-01-24 16:44:28 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
         <title>Simple ECS Microservice App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #008000F;}</style>
+        <style>body {margin-top: 40px; background-color: #0EF318;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
